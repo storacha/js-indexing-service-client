e6d5fc54e96dc9fd2f1bb16d82af75db04405b6d
ash <alan@storacha.network>
2024-11-07 11:02:44 +0000

    feat: query result create and archive (#2)
    Adds tooling to create and archive query results.

---
+++ test/query-result.spec.js
@@ -0,0 +1,32 @@
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import { describe, it } from 'mocha'
+import { assert } from 'chai'
+import * as QueryResult from '../src/query-result.js'
+
+describe('query result', () => {
+  it('round trip', async () => {
+    const digestString = 'zQmRm3SMS4EbiKYy7VeV3zqXqzyr76mq9b2zg3Tij3VhKUG'
+    const fixturePath = path.join(import.meta.dirname, 'fixtures', `${digestString}.queryresult.car`)
+    const carBytes = await fs.promises.readFile(fixturePath)
+
+    const extract0 = await QueryResult.extract(carBytes)
+    assert(extract0.ok)
+    assert(!extract0.error)
+
+    assert(extract0.ok.claims.size > 0)
+    assert(extract0.ok.indexes.size > 0)
+
+    const archive = await extract0.ok.archive()
+    assert(archive.ok)
+    assert(!archive.error)
+
+    const extract1 = await QueryResult.extract(archive.ok)
+    assert(extract1.ok)
+    assert(!extract1.error)
+
+    assert.equal(extract0.ok.root.toString(), extract1.ok.root.toString())
+    assert.equal(extract0.ok.claims.size, extract1.ok.claims.size)
+    assert.equal(extract0.ok.indexes.size, extract1.ok.indexes.size)
+  })
+})
